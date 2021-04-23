69dd4c44dba83f747efd298916c8a1bfbc35b045
Patrick McKinney <pnmcartography@gmail.com>
2021-04-23 14:56:47 -0400

    Added additional years to select from
    open year selector on page load

--- js/functions.js
+++ js/functions.js
@@ -9,9 +9,7 @@ const getUserSelectedYears = (webmap, webmapView, yearsArray, referenceLayer, sw
 
     if (swipeWidgetFind !== null) {
         webmapView.ui.remove('testSwipeWidget');
-        //webmapView.ui.destroy('testSwipeWidget');
     }
-    //console.log(swipeWidgetFind);
 
     // year 1 selected by user
     const year1Select = document.getElementById('year1Select').value;
@@ -22,34 +20,58 @@ const getUserSelectedYears = (webmap, webmapView, yearsArray, referenceLayer, sw
     let year2;
 
     switch(year1Select) {
-        case '2012':
+        case '1998':
             addLayerToMap(webmap, yearsArray[0]);
             year1 = yearsArray[0];
             break;
-        case '2016':
+        case '2003':
             addLayerToMap(webmap, yearsArray[1]);
             year1 = yearsArray[1];
             break;
-        case '2020':
+        case '2008':
             addLayerToMap(webmap, yearsArray[2]);
             year1 = yearsArray[2];
+            break;
+        case '2012':
+            addLayerToMap(webmap, yearsArray[3]);
+            year1 = yearsArray[3];
+            break;
+        case '2016':
+            addLayerToMap(webmap, yearsArray[4]);
+            year1 = yearsArray[4];
+            break;
+        case '2020':
+            addLayerToMap(webmap, yearsArray[5]);
+            year1 = yearsArray[5];
             break;
         default:
             console.warn('no appropriate year1 selected');
     }
 
     switch(year2Select) {
-        case '2012':
+        case '1998':
             addLayerToMap(webmap, yearsArray[0]);
             year2 = yearsArray[0];
             break;
-        case '2016':
+        case '2003':
             addLayerToMap(webmap, yearsArray[1]);
             year2 = yearsArray[1];
             break;
-        case '2020':
+        case '2008':
             addLayerToMap(webmap, yearsArray[2]);
             year2 = yearsArray[2];
+            break;
+        case '2012':
+            addLayerToMap(webmap, yearsArray[3]);
+            year2 = yearsArray[3];
+            break;
+        case '2016':
+            addLayerToMap(webmap, yearsArray[4]);
+            year2 = yearsArray[4];
+            break;
+        case '2020':
+            addLayerToMap(webmap, yearsArray[5]);
+            year2 = yearsArray[5];
             break;
         default:
             console.warn('no appropriate year2 selected');
--- js/map.js
+++ js/map.js
@@ -5,79 +5,83 @@ require([
   'esri/widgets/Swipe',
   'esri/layers/TileLayer'
   ],
-function(Map, MapView, Home, Swipe, TileLayer) {
-  // DOM Elements
-  // placeholder areas for years display for users
-  const year1UI = document.getElementById('year1display');
-  const year2UI = document.getElementById('year2display');
-  // select years element
-  const selectSubmitEl = document.getElementById('selectYearsSubmit');
-  // add event listenere
-  selectSubmitEl.addEventListener('click', function() {
-    getUserSelectedYears(map, mapView, arrayOfYears, refLayer, Swipe);
-    setImageryLabels(year1UI, year2UI);
-  });
+  function(Map, MapView, Home, Swipe, TileLayer) {
+    // DOM Elements
+    // placeholder areas for years display for users
+    const year1UI = document.getElementById('year1display');
+    const year2UI = document.getElementById('year2display');
+    // select years element
+    const selectSubmitEl = document.getElementById('selectYearsSubmit');
+    // add event listenere
+    selectSubmitEl.addEventListener('click', function() {
+      getUserSelectedYears(map, mapView, arrayOfYears, refLayer, Swipe);
+      setImageryLabels(year1UI, year2UI);
+      $('#selectModal').modal('hide');
+    });
 
-  // webmap object
-  const map = new Map({});
+    // webmap object
+    const map = new Map({});
 
-  // map view object
-  const mapView = new MapView({
-    map: map,
-    container: "map",
-    zoom: 0,
-    center: [-77.25, 40.15]
-  });
+    // map view object
+    const mapView = new MapView({
+      map: map,
+      container: "map",
+      zoom: 0,
+      center: [-77.25, 40.15]
+    });
 
-  // 2012 imagery > cached map service
-  const img2012 = new TileLayer({
-    url: "https://gis.ccpa.net/arcgiswebadaptor/rest/services/Imagery/Imagery2012/MapServer",
-    title: '2012'
-  });
+    // 1998 imagery > cached map service
+    const img1998 = new TileLayer({
+      url: "https://gis.ccpa.net/arcgiswebadaptor/rest/services/Imagery/Imagery1998/MapServer",
+      title: '1998'
+    });
 
-  // 2016 imagery > cached map service
-  const img2016 = new TileLayer({
-    url: "https://gis.ccpa.net/arcgiswebadaptor/rest/services/Imagery/Imagery2016/MapServer",
-    title: '2016'
-  });
+    // 2003 imagery > cached map service
+    const img2003 = new TileLayer({
+      url: "https://gis.ccpa.net/arcgiswebadaptor/rest/services/Imagery/Imagery2003/MapServer",
+      title: '2003'
+    });
 
-  // 2020 imagery > cached map service
-  const img2020 = new TileLayer({
-    url: "https://gis.ccpa.net/arcgiswebadaptor/rest/services/Imagery/Imagery2020/MapServer",
-    title: '2020'
-  });
+    // 2008 imagery > cached map service
+    const img2008 = new TileLayer({
+      url: "https://gis.ccpa.net/arcgiswebadaptor/rest/services/Imagery/Imagery2008/MapServer",
+      title: '2008'
+    });
 
-  // roads & municipal boundaries
-  // https://gis.ccpa.net/arcgiswebadaptor/rest/services/Property_Assessment/Roads_Municipal_Boundaries/MapServer
-  const refLayer = new TileLayer({
-    url: "https://gis.ccpa.net/arcgiswebadaptor/rest/services/Property_Assessment/Roads_Municipal_Boundaries/MapServer",
-    title: 'Reference Data'
-  });
+    // 2012 imagery > cached map service
+    const img2012 = new TileLayer({
+      url: "https://gis.ccpa.net/arcgiswebadaptor/rest/services/Imagery/Imagery2012/MapServer",
+      title: '2012'
+    });
 
-  const arrayOfYears = [img2012, img2016, img2020];
+    // 2016 imagery > cached map service
+    const img2016 = new TileLayer({
+      url: "https://gis.ccpa.net/arcgiswebadaptor/rest/services/Imagery/Imagery2016/MapServer",
+      title: '2016'
+    });
 
-  const homeWidget = new Home({
-    view: mapView,
-    label: 'Initial Map Extent'
-  });
+    // 2020 imagery > cached map service
+    const img2020 = new TileLayer({
+      url: "https://gis.ccpa.net/arcgiswebadaptor/rest/services/Imagery/Imagery2020/MapServer",
+      title: '2020'
+    });
 
-  // adds the home widget to the top left corner of the MapView
-  mapView.ui.add(homeWidget, "top-left");
+    // roads & municipal boundaries
+    // https://gis.ccpa.net/arcgiswebadaptor/rest/services/Property_Assessment/Roads_Municipal_Boundaries/MapServer
+    const refLayer = new TileLayer({
+      url: "https://gis.ccpa.net/arcgiswebadaptor/rest/services/Property_Assessment/Roads_Municipal_Boundaries/MapServer",
+      title: 'Reference Data'
+    });
 
-  /* Initial State of App */
-  // use pre-defined years?
-  // or let user select
-  //addLayerToMap(map, img2012);
-  //addLayerToMap(map, img2020);
-  //addLayerToMap(map, refLayer);
+    // list of imagery years
+    const arrayOfYears = [img1998, img2003, img2008, img2012, img2016, img2020];
 
-  // create Swipe Widget
-  //let swipeComponent = createSwipeWidget(Swipe, mapView, img2012, img2020, refLayer);
+    // map initial extent widget
+    const homeWidget = new Home({
+      view: mapView,
+      label: 'Initial Map Extent'
+    });
 
-  // add swipe ui to app
-  //mapView.ui.add(swipeComponent);
-
-  // set labels for years
-  //setImageryLabels(swipeComponent, year1UI, year2UI);
-
+    // adds the home widget to the top left corner of the MapView
+    mapView.ui.add(homeWidget, "top-left");
 });
